aef390615d680cc3f16d88e6d1c78a5838e36a94
Fantomka <43787512+Fantomka@users.noreply.github.com>
2019-04-17 00:13:25 +0300

    Add files via upload

---
+++ cource_work2/main.py
@@ -0,0 +1,243 @@
+"""
+Курсовой проект
+По теме: разработка программы решения системы линейный уравнений:
+    - методом главных элементов
+    - методом разложения на произведение двух треугольных матриц
+Язык: Python 3.7
+Среда: PyCharm
+Название программы: main.py
+Разработал: Фомченков С.Д.
+Дата: 31.03.2019
+Версия: v 1.0
+
+Задание:
+Разработать программу, решающее систему линейных алгебраических уравнений
+методам главных элементов и методом разложения на произведение двух треугольных матриц
+
+Описание алгоритма:
+1)инициализация системы
+2)ввод данных
+3)выбор метода
+4)вывод результатов вычислений
+
+Функции:
+set_size - Ввод данных от пользователя
+set_values - Ввод коэффициентов и свободных членов
+generate_rand_matrix - генерирует случайные числа для коэффициентов и свободных членов в диапозоне
+set_epsilon - Ввод точности вычисления и количество знаков после запятой в выводе
+initialization - инициадизация системы
+print_system - вывод системы
+gauss_main_elem - метод главного элемента
+multiply_line_by_number - Умножение строки на число
+adding_lines - прибавляет к строке 1 строку 2, умноженную на число
+LU_method - Метод разложения матрицы на произведение двух треуголььных матриц
+
+Использованные переменные:
+n - размерность матрицы
+a - матрица коэффициентов
+y - вектор свободных членов
+epsilon - точность вычислений
+answer - решение пользователя какой метод использовать
+"""
+
+from random import randrange
+
+
+def set_size():
+    """
+    Ввод данных от пользователя
+    """
+    while True:
+        try:
+            n = int(input("Введите размерность матрицы, это целое число большее 1 и меньшее 7: "))
+            if n <= 1 or n >= 7:
+                continue
+        except ValueError as error:
+            print(error)
+            continue
+        else:
+            return n
+
+
+def set_epsilon():
+    """
+    Ввод точности вычисления и количество знаков после запятой в выводе
+    """
+    while True:
+        try:
+            eps = int(input('Задайте точность (количество чисел после запятой): '))
+        except ValueError as error:
+            print(error)
+        else:
+            break
+    return eps
+
+
+def initialization(size):
+    a = [[0 for _ in range(size)] for _ in range(size)]  # инициализация
+    y = [0 for _ in range(size)]
+    return a, y
+
+
+def set_values(size):
+    """
+    Ввод коэффициентов и свободных членов
+    :param size: размер матрицы
+    :return: ссылки на объекты a и y
+    """
+    for i in range(size):
+        for j in range(size):
+            while True:
+                try:
+                    a[i][j] = float(input(f'A[{i+1}, {j+1}]: '))
+                except ValueError as error:
+                    print(error)
+                    continue
+                break
+        while True:
+            try:
+                y[i] = float(input(f'Y[{i+1}, {size+1}]: '))
+            except ValueError as error:
+                print(error)
+                continue
+            break
+    return a, y
+
+
+def generate_rand_matrix(a, y, n, low=0, high=10):
+    """
+    генерирует случайные числа для коэффициентов и свободных членов в диапозоне
+    :param low: нижняя граница интервала
+    :param high: верхняя граница интервала
+    """
+    for i in range(n):
+        for j in range(n):
+            a[i][j] = randrange(low, high)
+    y = [randrange(low, high) for _ in range(n)]
+    return a, y
+
+
+def print_system(a, y, n):
+    """
+    Вывод матрицы
+    """
+    for i in range(n):
+        for j in range(n):
+            print(f'{a[i][j]}*x{j}', end='')
+            if j < n-1:
+                print(' + ', end='')
+        print(f' = {y[i]}')
+    print()
+
+
+def gauss_main_elem(a, y, n, eps):
+    x = [0 for _ in range(n)]
+    eps = 1/(10**eps)  # точность
+    k = 0
+    while k < n:
+        # поиск строки с максимальным a[i][k]
+        maxi = abs(a[k][k])
+        index = k
+        for i in range(k+1, n):
+            if abs(a[i][k]) > maxi:
+                maxi = abs(a[i][k])
+                index = i
+        # Перестановка строк
+        if maxi < eps:
+            print("Решение получить невозможно из-за нулевого столбца ")
+            print(index, " матрицы A")
+            return 0
+        for j in range(n):
+            temp = a[k][j]
+            a[k][j] = a[index][j]
+            a[index][j] = temp
+        temp = y[k]
+        y[k] = y[index]
+        y[index] = temp
+        # Нормализация уравнений
+        for i in range(k, n):
+            temp = a[i][k]
+            if abs(temp) < eps:
+                continue  # для нулевого коэффициента пропустить
+            for j in range(n):
+                a[i][j] = a[i][j] / temp
+            y[i] = y[i] / temp
+            if i == k:
+                continue  # уравнение не вычитать само из себя
+            for j in range(n):
+                a[i][j] = a[i][j] - a[k][j]
+            y[i] = y[i] - y[k]
+        k = k + 1
+    # обратная подстановка
+    for k in range(n-1, -1, -1):
+        x[k] = y[k]
+        for i in range(k):
+            y[i] = y[i] - a[i][k] * x[k]
+    return x
+
+
+def multiply_line_by_number(a, y, line_index, number, n):
+    """
+    Умножение строки на число
+    :param a: матрица коэффициентов
+    :param y: список свободных членов
+    :param line_index: номер строки
+    :param number: число
+    :param n: размерность матрицы
+    """
+    if number != 0:
+        for elem_index in range(n):
+            a[line_index][elem_index] *= number
+        y[line_index] *= number
+    else:
+        print("Число не должно быть нулем")
+    return a, y
+
+
+def adding_lines(a, y, n, i1, i2, number):
+    """
+    прибавляет к строке 1 строку 2, умноженную на число
+    """
+    for j in range(n):
+        a[i1][j] += a[i2][j] * number
+    y[i1] += y[i2] * number
+    return a, y
+
+
+def LU_method(a, y, n):
+    """
+    Метод разложения матрицы на произведение двух треуголььных матриц
+    :return: список неизвестных
+    """
+    print_system(a, y, n)
+    for i in range(n):
+        a, y = multiply_line_by_number(a, y, i, 1/a[i][i], n)
+        for j in range(i+1, n):
+            a, y = adding_lines(a, y, n, j, i, -a[j][i])
+        for j in range(i-1, -1, -1):
+            a, y = adding_lines(a, y, n, j, i, -a[j][i])
+    return y
+
+
+if __name__ == '__main__':
+    n = set_size()
+    epsilon = set_epsilon()
+    a, y = initialization(n)
+    #a, y = set_values(n)
+    a, y = generate_rand_matrix(a, y, n, 11, 18)
+    print_system(a, y, n)
+    LU_method(a, y, n)
+    print("1 - метод главных элементов")
+    print("2 - метод разложения на произведение двух треугольных матриц")
+    while True:
+        chose = input("Выбирете метод (введите цифру): ")
+        if chose == '1':
+            for elem in gauss_main_elem(a, y, n, epsilon):
+                print(f'X = {elem:^5.{epsilon}f}', end='')
+            break
+        elif chose == '2':
+            for elem in LU_method(a, y, n):
+                print(f'X = {elem:^5.{epsilon}f}', end='')
+            break
+        else:
+            print('Некорректный ввод, попробуйте снова')
---
+++ course_work/class_matrix.py
@@ -0,0 +1,265 @@
+"""
+Course project
+This program should find a solution of linear algebraic equations system
+using methods of Jordana-Gauss and Gauss-Seidel
+"""
+
+from random import randrange, uniform
+from math import sqrt
+
+
+class Matrix(list):
+    """
+    This class using for collect information and methods about
+    systems of linear algebraic equations and this class
+    inheritance methods from class list
+    """
+    def __init__(self, size=0):
+        """
+        Initialization of matrix
+        :param size: dimension of quadratic matrix
+        """
+        self.__dimension = size
+        self.coefficients = [[0 for _ in range(size)] for _ in range(size)]
+        self.answers = [0 for _ in range(size)]
+        self.epsilon = 1
+
+    def get_coefficients(self) -> list:
+        """
+        :return: coefficients
+        """
+        return self.coefficients
+
+    def get_answers(self) -> list:
+        """
+        :return: answers
+        """
+        return self.answers
+
+    def get_extended_matrix(self) -> list:
+        """
+        :return: extended matrix with coefficients and answers
+        (list[dimension+1][dimension])
+        """
+        out = self.coefficients
+        out.append(self.answers)
+        return out
+
+    def set_size_user_mode(self):
+        """
+        write invitation to set dimension of matrix
+        """
+        while True:
+            try:
+                self.__dimension = int(input("Введите размерность матрицы, это целое число большее 1 и меньшее 7: "))
+                if self.__dimension <= 1 or self.__dimension >= 7:
+                    continue
+            except ValueError as error:
+                print(error)
+                continue
+            else:
+                self.coefficients = [[0 for _ in range(self.__dimension)] for _ in range(self.__dimension)]
+                self.answers = [0 for _ in range(self.__dimension)]
+                break
+
+    def set_values_user_mode(self):
+        for line in range(self.__dimension):
+            for column in range(self.__dimension):
+                while True:
+                    try:
+                        self.coefficients[line][column] = float(input(f'   Matrix[{line+1}, {column+1}]: '))
+                    except ValueError as error:
+                        print(error)
+                        continue
+                    break
+            while True:
+                try:
+                    self.answers[line] = float(input(f'Matrix[{line+1}, {self.__dimension+1}]: '))
+                except ValueError as error:
+                    print(error)
+                    continue
+                break
+
+    def set_epsilon_user_mode(self):
+        """
+        set number of digits after pointer
+        """
+        while True:
+            try:
+                self.epsilon = int(input('Задайте точность (количество чисел после запятой): '))
+            except ValueError as error:
+                print(error)
+            else:
+                break
+
+    def set_coefficients(self, coefs: list):
+        """
+        set coefficients list needs
+        :param coefs: list of lists of coefficients
+        :return:
+        """
+        self.coefficients = coefs
+
+    def set_answers(self, ans: list):
+        """
+        set answers, list needs
+        :param ans: list of answers
+        """
+        self.answers = ans
+
+    def generate_rand_integer_matrix(self, low=0, high=10):
+        """
+        generation random integer coefficients and answers
+        :param low: left bound of the set
+        :param high: right bound of the set
+        """
+        for line in range(self.__dimension):
+            for column in range(self.__dimension):
+                self.coefficients[line][column] = randrange(low, high)
+        self.answers = [randrange(low, high) for _ in range(self.__dimension)]
+
+    def generate_rand_float_matrix(self, low=0, high=10):
+        """
+        generation random integer coefficients and answers
+        :param low: left bound of the set
+        :param high: right bound of the set
+        """
+        for line in range(self.__dimension):
+            for column in range(self.__dimension):
+                self.coefficients[line][column] = round(uniform(low, high), self.epsilon)
+        self.answers = [round(uniform(low, high), self.epsilon) for _ in range(self.__dimension)]
+
+    def print_matrix(self):
+        """
+        simple printing matrix of coefficients in command line
+        """
+        for line in self.coefficients:
+            for elem in line:
+                print(f'{elem:<{self.epsilon + 2}.{self.epsilon}f}', end='')
+            print()
+        print()
+
+    def print_extended_matrix(self):
+        """
+        simple printing extended matrix in command line
+        """
+        for line_index in range(self.__dimension):
+            print('|', end='')
+            for column_index in range(self.__dimension):
+                print(f'{self.coefficients[line_index][column_index]:^{self.epsilon + 5}.{self.epsilon}f}', end='')
+            if self.__dimension // 2 is line_index:
+                print(f'| = |{self.answers[line_index]:^{self.epsilon + 5}.{self.epsilon}f}|')
+            else:
+                print(f'|   |{self.answers[line_index]:^{self.epsilon + 5}.{self.epsilon}f}|')
+        print()
+
+    def swap_lines(self, index_1=0, index_2=0):
+        """
+        swapping lines (coefficients and answers)
+        :param index_1: index of line 1
+        :param index_2: index of line 2
+        """
+        temp = self.coefficients[index_1]
+        self.coefficients[index_1] = self.coefficients[index_2]
+        self.coefficients[index_2] = temp
+
+        temp = self.answers[index_1]
+        self.answers[index_1] = self.answers[index_2]
+        self.answers[index_2] = temp
+
+    def swap_columns(self, index_1=0, index_2=0, ans_swap=False):
+        """
+        This method swaps columns in matrix
+        :param index_1: index of first column
+        :param index_2: index of second column
+        :param ans_swap: bool variable. If it's True, swapping first column with column of answers
+        """
+        if ans_swap:
+            for i in range(self.__dimension):
+                temp = self.coefficients[i][index_1]
+                self.coefficients[i][index_1] = self.answers[i]
+                self.answers[i] = temp
+        else:
+            for i in range(self.__dimension):
+                temp = self.coefficients[i][index_1]
+                self.coefficients[i][index_1] = self.coefficients[i][index_2]
+                self.coefficients[i][index_2] = temp
+
+    def adding_lines(self, addend_1, addend_2, number):
+        """
+        adding line 1 with line 2 multiplied on number
+        result gets line 1
+        :param addend_1: index line 1
+        :param addend_2: index line 2
+        :param number: coefficient
+        """
+        for elem_index in range(self.__dimension):
+            self.coefficients[addend_1][elem_index] += self.coefficients[addend_2][elem_index] * number
+        self.answers[addend_1] += self.answers[addend_2] * number
+
+    def multiply_line_by_number(self, line_index, number):
+        """
+        multiply line by number
+        :param line_index: index of line
+        :param number: value of number
+        """
+        if number != 0:
+            for elem_index in range(self.__dimension):
+                self.coefficients[line_index][elem_index] *= number
+            self.answers[line_index] *= number
+        else:
+            print("Число не должно быть нулем")
+
+    def set_not_zero_diagonal_element(self, index=0):
+        """
+        check in first line first element, if it is zero,
+        tries to swap with line that have not zero element in first place
+        """
+        if self.coefficients[index][index]:  # check is elem != 0
+            return
+        for line_index in range(index + 1, len(self.coefficients)):
+            if self.coefficients[line_index][index]:
+                self.swap_lines(index, line_index)
+                break
+            else:
+                continue
+
+    def jordana_gauss_method(self):
+        """
+        this method tries to find list of roots of the equation
+        Using Jordana-Gauss method
+        After using this method matrix will become unit matrix
+        :return: list of roots of the equation
+        """
+        for diagonal_index in range(self.__dimension):
+            self.set_not_zero_diagonal_element(diagonal_index)
+            self.multiply_line_by_number(diagonal_index, 1/self.coefficients[diagonal_index][diagonal_index])
+            for under_index in range(diagonal_index + 1, self.__dimension):
+                self.adding_lines(under_index, diagonal_index, -self.coefficients[under_index][diagonal_index])
+            for upper_index in range(diagonal_index - 1, -1, -1):
+                self.adding_lines(upper_index, diagonal_index, -self.coefficients[upper_index][diagonal_index])
+            self.print_extended_matrix()
+        return self.answers
+
+    def gauss_seidel_method(self):
+        """
+        this method tries to find list of roots of the equation
+        Using gauss_seidel method
+        converge - condition for terminating the computation of the system according to the seidel
+        current: list of x values on current step
+        previous: list of x values on previous step
+        :return: list of roots of the equation
+        """
+        n = self.__dimension
+        previous = [.0 for _ in range(n)]
+
+        converge = False
+        while not converge:
+            current = previous[:]
+            for line in range(n):
+                sum1 = sum(self.coefficients[line][column] * current[column] for column in range(line))
+                sum2 = sum(self.coefficients[line][column] * previous[column] for column in range(line+1, n))
+                current[line] = (self.answers[line] - sum1 - sum2) / self.coefficients[line][line]
+            converge = sqrt(sum((current[line] - previous[line]) ** 2 for line in range(n))) <= 1/(10 ** self.epsilon)
+            previous = current[:]
+        return previous
